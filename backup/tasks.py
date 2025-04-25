603db059100af08b74d1348a8bf795cee0482237
delcain <diego@cuidadodigital.com.br>
2025-04-25 16:09:26 -0300

    Envia E-mail para Tarefa agendada

--- backup/tasks.py
+++ backup/tasks.py
@@ -1,6 +1,11 @@
 from celery import shared_task
+from django.conf import settings
+import traceback
+
 from .models import MikrotikDevice, Backup
 from .utils import backup_device
+from django.core.mail import send_mail
+
 
 @shared_task
 def async_backup_device(device_id):
@@ -10,7 +15,24 @@ def async_backup_device(device_id):
 
 @shared_task
 def schedule_all_backups(*args, **kwargs):
-    from .models import MikrotikDevice
-    for device in MikrotikDevice.objects.all():
-        device_id = device.id
-        async_backup_device.delay(device_id)
+    try:
+        from .models import MikrotikDevice
+        for device in MikrotikDevice.objects.all():
+            device_id = device.id
+            async_backup_device.delay(device_id)
+
+            send_mail(
+            subject=f'[Backup Mikrotik] Tarefa iniciada com sucesso',
+            message=f'backups iniciados com sucesso.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[admin_email for _, admin_email in settings.ADMINS],
+        )
+    except Exception as e:
+        error_msg = traceback.format_exc()
+        send_mail(
+            subject=f'[Backup Mikrotik] Erro ao iniciar tarefa de backup',
+            message=f'Erro: {error_msg}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[admin_email for _, admin_email in settings.ADMINS],
+        )
+        raise
